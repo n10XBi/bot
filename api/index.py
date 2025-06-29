f84a4aee4e76b0c0c7d8b91df509a3c86b62e1bd
n10XBi <164579514+n10XBi@users.noreply.github.com>
2025-06-29 15:56:55 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 from threading import Thread
-from keep_alive import keep_alive
+import sys
+import os
+
+# Inject path biar bisa impor dari root
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from main import run_bot
+from keep_alive import keep_alive
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template("i.html", active_since="2025-06-28 23:55:00")
+    return render_template("i.html", active_since="2025-06-28 23:59:00")
 
 def handler(request):
     Thread(target=keep_alive).start()
