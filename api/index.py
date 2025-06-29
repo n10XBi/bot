8ad888ed06fc54cf55caf672316b901eff45ea7c
n10XBi <164579514+n10XBi@users.noreply.github.com>
2025-06-29 15:38:51 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
-from main import run_bot  # dari main.py kamu yang sudah diedit
+from flask import Flask, render_template
+from threading import Thread
 from keep_alive import keep_alive
+from main import run_bot
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("i.html", active_since="2025-06-28 23:55:00")
 
 def handler(request):
-    keep_alive()
-    run_bot()
+    Thread(target=keep_alive).start()
+    Thread(target=run_bot).start()
     return "Bot is running!", 200
